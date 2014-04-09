375aa2847a28bb8774542845d3857569dc8a3b0a
Palash Nandy <palash@media.mit.edu>
2014-04-09 14:19:49 -0400

    ros initialization code

---
+++ machine/ubuntu-12.04/initialization.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu precise main" > /etc/apt/sources.list.d/ros-latest.list'
+wget http://packages.ros.org/ros.key -O - | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install ros-hydro-desktop-full
+sudo rosdep init
+rosdep update
