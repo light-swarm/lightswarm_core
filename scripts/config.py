8a2b52e5b06ea3c5ca04b9af04039a1c83807ed7
kbl <kyubok@gmail.com>
2014-04-09 13:43:07 -0400

    changes to config.py

--- scripts/config.py
+++ scripts/config.py
@@ -1,9 +1,10 @@
 
-
 world_dimensions = [200, 200]
 
 ## april_detector calibration
 sense_camera_points = np.float32(np.array([[0,  20, 180], [100,   20, 180], [0, 20, 280], [100, 20, 280]]))
 sense_world_points = np.float32(np.array([[0, -100,  20], [100, -100,  20], [0,  0,  20], [100,  0,  20]]))
 
+## for shadow computation
+projector_locations = [('first', (100, 100, 500)), ('second', (-100, -100, 500))]
 
