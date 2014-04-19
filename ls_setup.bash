d987403a7c320ef3d6c2e65eb19541d76922e0d2
Charles Rose <crose24@gmail.com>
2014-04-19 13:59:22 -0400

    removed an echo

--- ls_setup.bash
+++ ls_setup.bash
@@ -3,8 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/../../devel/setup.bash
 
-echo $PYTHONPATH
-
 opencv_path=/usr/local/lib/python2.7/dist-packages
 
 export PYTHONPATH=$opencv_path:${PYTHONPATH/$opencv_path}
